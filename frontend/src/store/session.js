da2dda59d80f424f9d14d0b4ea05ac753fdb12ef
Adam <warwickjunglex@gmail.com>
2020-11-19 15:54:14 -0500

    Session working

---
+++ frontend/src/store/session.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { fetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+function setUser(user) {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
+
+function removeUser() {
+    return {
+        type: REMOVE_USER,
+    };
+};
+
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const res = await fetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
+    });
+    dispatch(setUser(res.data.user));
+    return res;
+}
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch(action.type) {
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
